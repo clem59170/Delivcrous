1eb8f39ee07ef64b73e85eec37a4d3338d8774d4
crafa <clementrafa@gmail.com>
2023-08-30 09:07:20 +0200

    first commit

---
+++ src/main/java/com/example/delivcrous/DelivcrousApplication.java
@@ -0,0 +1,13 @@
+package com.example.delivcrous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivcrousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelivcrousApplication.class, args);
+    }
+
+}
