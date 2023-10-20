03d0155677f8ad782086da9f34f3c986a7010a99
crafa <clementrafa@gmail.com>
2023-10-20 12:01:33 +0200

    avoiding bad user to access another user data (command for example)

--- src/main/java/com/example/delivcrous/controller/CommandeController.java
+++ src/main/java/com/example/delivcrous/controller/CommandeController.java
@@ -1,6 +1,7 @@
 package com.example.delivcrous.controller;
 
 import com.example.delivcrous.model.Commande;
+import com.example.delivcrous.repository.CommandeRepository;
 import com.example.delivcrous.service.CommandeService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
@@ -15,12 +16,19 @@ public class CommandeController {
     private CommandeService commandeService;
 
     @GET
+    @Path("/getallcommandes")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Commande> getAllCommandes() {
         return commandeService.getAllCommandes();
     }
 
+    @GET
+    @Path("/getcommande")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Commande> getCommandeByUser_id(@QueryParam("user_id") Long user_id) { return commandeService.findByUserId(user_id); }
+
     @POST
+    @Path("/createcommande")
     @Consumes(MediaType.APPLICATION_JSON)
     public void createCommande(Commande commande, @QueryParam("user_id") Long user_id) {
         commandeService.createCommande(commande, user_id);
--- src/main/java/com/example/delivcrous/repository/CommandeRepository.java
+++ src/main/java/com/example/delivcrous/repository/CommandeRepository.java
@@ -1,9 +1,18 @@
 package com.example.delivcrous.repository;
 
 import com.example.delivcrous.model.Commande;
+import com.example.delivcrous.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
+    @Query("SELECT c FROM Commande c JOIN c.utilisateur u WHERE u.user_id = :userId")
+    List<Commande> findByUserId(@Param("userId") Long userId);
+
 }
+
