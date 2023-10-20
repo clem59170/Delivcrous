eef7c36ff9d7fb67051651d58e230ccdafa2ac72
crafa <clementrafa@gmail.com>
2023-10-20 23:09:42 +0200

    adding solde_crous adresse and tel in infos returned when logging

--- src/main/java/com/example/delivcrous/payload/response/UserInfoResponse.java
+++ src/main/java/com/example/delivcrous/payload/response/UserInfoResponse.java
@@ -54,5 +54,4 @@ public class UserInfoResponse {
     public Double getSolde_crous() { return solde_crous; }
 
     public void setSolde_crous(Double solde_crous) {this.solde_crous = solde_crous; }
-
 }
