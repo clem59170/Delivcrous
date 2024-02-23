60bfdf20a9a6c6bcf1b1b91c9b5d689aabafb867
clem59170 <82813607+clem59170@users.noreply.github.com>
2024-02-23 17:18:06 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/example/delivcrous/security/WebSecurityConfig.java
+++ src/main/java/com/example/delivcrous/security/WebSecurityConfig.java
@@ -24,6 +24,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import java.util.List;
+
+//
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
